c07a77aa2c048b7a13ea108adb25f05b502598b7
Manjeet Kumar <mkj22834@gmail.com>
2026-07-23 20:49:28 +0530

    prisma setup completed

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import AboutComponent from '@/components/about'
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <AboutComponent></AboutComponent>
+    </div>
+  )
+}
+
+export default About
